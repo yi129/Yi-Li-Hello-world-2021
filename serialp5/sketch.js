e7017b21ebeac0be31c31dd4ce8659f03d21505b
yli414 <91208241+yi129@users.noreply.github.com>
2021-10-12 00:55:33 -0400

    test 1

---
+++ serialp5/sketch.js
@@ -0,0 +1,69 @@
+let serial;
+let latestData = "waiting for data";
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+
+ serial = new p5.SerialPort();
+
+ serial.list();
+ serial.open('/dev/tty.usbserial-1420');
+
+ serial.on('connected', serverConnected);
+
+ serial.on('list', gotList);
+
+ serial.on('data', gotData);
+
+ serial.on('error', gotError);
+
+ serial.on('open', gotOpen);
+
+ serial.on('close', gotClose);
+}
+
+function serverConnected() {
+ print("Connected to Server");
+}
+
+function gotList(thelist) {
+ print("List of Serial Ports:");
+
+ for (let i = 0; i < thelist.length; i++) {
+  print(i + " " + thelist[i]);
+ }
+}
+
+function gotOpen() {
+ print("Serial Port is Open");
+}
+
+function gotClose(){
+ print("Serial Port is Closed");
+ latestData = "Serial Port is Closed";
+}
+
+function gotError(theerror) {
+ print(theerror);
+}
+
+function gotData() {
+ let currentString = serial.readLine();
+  trim(currentString);
+ if (!currentString) return;
+ console.log(currentString);
+ latestData = currentString;
+}
+
+function draw() {
+ background(220,0,200);
+ fill(0,0,0);
+ text(latestData, 10, 10);
+ //ellipse(50, 50, latestData, latestData);
+
+ if (serial.available() > 0) {
+  let data = serial.read();
+  ellipse(50,50,data,data);
+ }
+
+}
