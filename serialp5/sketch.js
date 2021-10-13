b4a30709804c6d6edd5f861322c4d4f4c2494d5a
yli414 <91208241+yi129@users.noreply.github.com>
2021-10-13 11:46:00 -0400

    hw week5

--- serialp5/sketch.js
+++ serialp5/sketch.js
@@ -1,13 +1,16 @@
 let serial;
 let latestData = "waiting for data";
 
+
 function setup() {
  createCanvas(windowWidth, windowHeight);
+ print("BEGIN!");
+ console.log("starting!");
 
  serial = new p5.SerialPort();
 
  serial.list();
- serial.open('/dev/tty.usbserial-1420');
+ serial.open('/dev/tty.usbserial-1410');
 
  serial.on('connected', serverConnected);
 
@@ -20,6 +23,10 @@ function setup() {
  serial.on('open', gotOpen);
 
  serial.on('close', gotClose);
+
+ 
+
+ 
 }
 
 function serverConnected() {
@@ -53,17 +60,60 @@ function gotData() {
  if (!currentString) return;
  console.log(currentString);
  latestData = currentString;
+
 }
 
 function draw() {
- background(220,0,200);
- fill(0,0,0);
- text(latestData, 10, 10);
- //ellipse(50, 50, latestData, latestData);
+    background(0,0,0);
+    //fill(latestData*2,latestData,latestData*2);
+    text(latestData, 10, 10);
+    noStroke();
+    //first option
+  for (let i = 0; i < 50; i++) {
+    fill(0, latestData*2, 255, 255);
+    ellipse(random(windowWidth), random(windowWidth), latestData*2,latestData*2); 
+    
+    //second option
+    //fill(0, latestData/2, 255);
+    //rect(200, 200, latestData*5, latestData*5);
+    
+    //fill(0, latestData, 255);
+    //rect(200, 200, latestData*4, latestData*4);
+    
+    //fill(0, latestData*2, 255);
+    //rect(200, 200, latestData*3, latestData*3);
+    
+    //fill(0, latestData*2+50, 255);
+    //rect(200, 200, latestData*2, latestData*2);
 
- if (serial.available() > 0) {
-  let data = serial.read();
-  ellipse(50,50,data,data);
- }
 
+    //third option
+    //ellipse(350, 350, latestData*2,latestData*2);
 }
+}
+    
+ 
+    
+
+
+
+
+
+
+    //lastestData=map(lastestData,0,1186,0,250);
+    //if (lastestData == 0) {
+   
+        //ellipse(width / 2,height /2 ,100,100);
+    //} else {
+       // rectMode(CENTER);
+       // rect(width / 2, height / 2,100,100);
+       //let m = map(latestData, 0, 1184, 0, 550);
+    
+       //ellipse(850,100,latestData/2,latestData/2);
+       //ellipse(10,100,latestData/2,latestData/2);
+       //ellipse(700,200,latestData/3,latestData/2);
+       //ellipse(latestData,latestData,50,50);
+       //ellipse(1000,100,latestData/2,latestData/2);
+
+       
+    
--- serialp5/sketch_oct10a/sketch_oct10a.ino
+++ serialp5/sketch_oct10a/sketch_oct10a.ino
@@ -11,29 +11,36 @@
 
 // defines variables
 long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+int distance; //
+//const int outputValue = 0; //variable for the distance measurement
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  //Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  //Serial.println("with Arduino UNO R3");
+  
 }
 void loop() {
   // Clears the trigPin condition
+  delay(50);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+  
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
+  
   // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  //Serial.print("Distance: ");
+  // Speed of sound wave divided by 2 (go and back)
+  distance = duration * 0.034 / 2; 
+
+  distance = constrain(distance, 5, 100);
+
   Serial.println(distance);
   //Serial.println(" cm");
 }
